d4ba3f98777bd209e8873c5fef802a66cee3e59f
Nicolai Haeni <haeni001@umn.edu>
2022-09-05 16:17:36 -0500

    Render rgbd images

--- create_dataset.py
+++ create_dataset.py
@@ -1,16 +1,17 @@
 import os
 
-os.environ["PYOPENGL_PLATFORM"] = "osmesa"
+# os.environ["PYOPENGL_PLATFORM"] = "egl"
 import json
-import argparse
 import h5py
+import random
+import argparse
 import numpy as np
 import trimesh
 
-# import open3d as o3d
-
+import utils
 from mesh_to_sdf.utils import scale_to_unit_sphere
 from mesh_to_sdf import get_surface_point_cloud, surface_point_cloud
+import pyrender
 
 
 categories = {
@@ -41,15 +42,10 @@ def main(args):
         category = key_list[val_list.index(category)]
         out_path = os.path.join(args.out_dir, category, fname)
         out_filename = os.path.join(out_path, f"{fname}.h5")
-
-        if os.path.exists(out_filename):
-            print(f"Skipping: {out_filename}")
-            continue
+        out_rgbd_path = os.path.join(out_path, f"{fname}_rgbd.h5")
 
         if not os.path.exists(out_path):
             os.makedirs(out_path)
-
-        print(f"Processing {ii}/{len(chunk)} files")
 
         mesh = trimesh.load(
             os.path.join(args.mesh_dir, filename, "models", "model_normalized.obj"),
@@ -63,48 +59,128 @@ def main(args):
 
         # Normalize the mesh in sphere of radius 1/1.03
         mesh = scale_to_unit_sphere(mesh)
-        bounding_radius = np.max(np.linalg.norm(mesh.vertices, axis=1)) * 1.1
 
-        # Get the surface point cloud
-        point_cloud = get_surface_point_cloud(mesh, "scan", bounding_radius)
-        surface_points, surface_normals = point_cloud.get_random_surface_points(500000)
+        # Create SDF data
+        if not os.path.exists(out_filename):
+            print(f"Processing {ii}/{len(chunk)} files")
+            bounding_radius = np.max(np.linalg.norm(mesh.vertices, axis=1)) * 1.1
 
-        # Free points
-        free_points = np.random.uniform(-1, 1, size=(500000, 3))
-        free_points_sdf = point_cloud.get_sdf_in_batches(
-            free_points, use_depth_buffer=True, sample_count=10000000
-        )
+            # Get the surface point cloud
+            point_cloud = get_surface_point_cloud(mesh, "scan", bounding_radius)
+            surface_points, surface_normals = point_cloud.get_random_surface_points(
+                500000
+            )
 
-        # # # Visualize
-        # pcd, free_pcd = o3d.geometry.PointCloud(), o3d.geometry.PointCloud()
-        # pcd.points = o3d.utility.Vector3dVector(surface_points)
-        # pcd.normals = o3d.utility.Vector3dVector(surface_normals)
-        # colors = np.zeros_like(surface_points)
-        # colors[:, 0] = 1
-        # pcd.colors = o3d.utility.Vector3dVector(colors)
+            # Free points
+            free_points = np.random.uniform(-1, 1, size=(500000, 3))
+            free_points_sdf = point_cloud.get_sdf_in_batches(
+                free_points, use_depth_buffer=True, sample_count=10000000
+            )
 
-        # free_pcd.points = o3d.utility.Vector3dVector(free_points)
-        # colors = np.zeros_like(free_points)
-        # colors[:, 1] = 1
-        # free_pcd.colors = o3d.utility.Vector3dVector(colors)
+            surface_data = np.concatenate([surface_points, surface_normals], axis=-1)
+            free_data = np.concatenate([free_points, free_points_sdf[:, None]], axis=-1)
 
-        # o3d.visualization.draw_geometries([pcd, free_pcd])
+            h5file = h5py.File(out_filename, "w")
+            h5file.create_dataset(
+                "free_pts",
+                data=free_data,
+                compression="gzip",
+            )
+            h5file.create_dataset(
+                "surface_pts",
+                data=surface_data,
+                compression="gzip",
+            )
+            h5file.close()
+        else:
+            print(f"Skipping: {out_filename}")
+        # Render RGBD images
+        if not os.path.exists(out_rgbd_path):
+            cam_locations = utils.sample_spherical(30, 3.0)
+            obj_location = np.zeros((1, 3))
+            cv_poses = utils.look_at(cam_locations, obj_location)
+            cam_locations = [utils.cv_cam2world_to_bcam2world(m) for m in cv_poses]
+            image_size = (256, 256)
+            camera = pyrender.IntrinsicsCamera(
+                fx=262.5, fy=262.5, cx=128.0, cy=128.0, znear=0.01, zfar=100
+            )
 
-        surface_data = np.concatenate([surface_points, surface_normals], axis=-1)
-        free_data = np.concatenate([free_points, free_points_sdf[:, None]], axis=-1)
+            scene = pyrender.Scene.from_trimesh_scene(
+                trimesh.Scene(mesh), ambient_light=(1, 1, 1)
+            )
 
-        h5file = h5py.File(out_filename, "w")
-        h5file.create_dataset(
-            "free_pts",
-            data=free_data,
-            compression="gzip",
-        )
-        h5file.create_dataset(
-            "surface_pts",
-            data=surface_data,
-            compression="gzip",
-        )
-        h5file.close()
+            rgbs = []
+            depths = []
+            masks = []
+            for ii, w2c in enumerate(cam_locations):
+                # Add camera roll
+                roll_matrix = utils.sample_roll_matrix(
+                    np.random.uniform(-1, 1, 1) * np.pi
+                )
+                w2c = roll_matrix @ w2c
+
+                if ii == 0:
+                    w2c0 = np.array(utils.get_world2cam_from_blender_cam(w2c))
+
+                if ii == 0:
+                    cam_node = scene.add(camera, pose=np.array(w2c))
+                else:
+                    scene.set_pose(cam_node, pose=np.array(w2c))
+
+                r = pyrender.OffscreenRenderer(*image_size)
+                color, depth = r.render(
+                    scene, flags=pyrender.constants.RenderFlags.FLAT
+                )
+
+                mask = depth != 0
+                depth[mask == 0.0] = 100
+
+                rgbs.append(color)
+                depths.append(depth)
+                masks.append(mask)
+                r.delete()
+
+            # for rgb, depth, mask in zip(rgbs, depths, masks):
+            # fig, ax = plt.subplots(1, 3)
+            # ax[0].imshow(rgb)
+            # ax[1].imshow(depth, cmap="plasma")
+            # ax[2].imshow(mask, cmap="plasma")
+            # plt.show()
+
+            rgbs = np.stack([r for r in rgbs])
+            depths = np.stack([r for r in depths])
+            masks = np.stack([r for r in masks])
+            hf = h5py.File(out_rgbd_path, "w")
+            hf.create_dataset("rgb", data=rgbs, compression="gzip", dtype="f")
+            hf.create_dataset("depth", data=depths, compression="gzip", dtype="f")
+            hf.create_dataset("mask", data=masks, compression="gzip", dtype="f")
+            hf.close()
+
+            # # Visualize
+            # pcd = o3d.geometry.PointCloud()
+            # pcd.points = o3d.utility.Vector3dVector(surface_points)
+            # pcd.normals = o3d.utility.Vector3dVector(surface_normals)
+            # colors = np.zeros_like(surface_points)
+            # colors[:, 0] = 1
+            # pcd.colors = o3d.utility.Vector3dVector(colors)
+
+            # depth = depths[0]
+            # depth[depth == 100] = np.inf
+            # u, v = np.where(depth != np.inf)
+            # x = (u - 128) / 262.5 * depth[u, v]
+            # y = (v - 128) / 262.5 * depth[u, v]
+            # z = depth[u, v]
+            # points = np.stack([x, y, z], axis=-1)
+
+            # pcd_part = o3d.geometry.PointCloud()
+            # pcd_part.points = o3d.utility.Vector3dVector(points)
+            # colors = np.zeros_like(points)
+            # colors[:, 1] = 1
+            # pcd_part.colors = o3d.utility.Vector3dVector(colors)
+
+            # axis = o3d.geometry.TriangleMesh.create_coordinate_frame()
+            # pcd_part.transform(np.linalg.inv(w2c0))
+            # o3d.visualization.draw_geometries([pcd, pcd_part, axis])
 
 
 if __name__ == "__main__":
---
+++ utils.py
@@ -0,0 +1,126 @@
+import numpy as np
+from mathutils import Matrix, Vector
+
+
+def normalize(vec):
+    return vec / (np.linalg.norm(vec, axis=-1, keepdims=True) + 1e-9)
+
+
+def sample_roll_matrix(theta):
+    return Matrix(
+        (
+            (np.cos(theta), -np.sin(theta), 0, 0),
+            (np.sin(theta), np.cos(theta), 0, 0),
+            (0, 0, 1, 0),
+            (0, 0, 0, 1),
+        )
+    )
+
+
+def sample_spherical(n, radius=1.0):
+    xyz = np.random.normal(size=(n, 3))
+    xyz = normalize(xyz) * radius
+    return xyz
+
+
+# All the following functions follow the opencv convention for camera coordinates.
+def look_at(cam_location, point):
+    # Cam points in positive z direction
+    forward = point - cam_location
+    forward = normalize(forward)
+
+    tmp = np.array([0.0, -1.0, 0.0])
+
+    right = np.cross(tmp, forward)
+    right = normalize(right)
+
+    up = np.cross(forward, right)
+    up = normalize(up)
+
+    mat = np.stack((right, up, forward, cam_location), axis=-1)
+
+    hom_vec = np.array([[0.0, 0.0, 0.0, 1.0]])
+
+    if len(mat.shape) > 2:
+        hom_vec = np.tile(hom_vec, [mat.shape[0], 1, 1])
+
+    mat = np.concatenate((mat, hom_vec), axis=-2)
+    return mat
+
+
+def cv_cam2world_to_bcam2world(cv_cam2world):
+    """
+    :cv_cam2world: numpy array.
+    :return:
+    """
+    R_bcam2cv = Matrix(((1, 0, 0), (0, -1, 0), (0, 0, -1)))
+
+    cam_location = Vector(cv_cam2world[:3, -1].tolist())
+    cv_cam2world_rot = Matrix(cv_cam2world[:3, :3].tolist())
+
+    cv_world2cam_rot = cv_cam2world_rot.transposed()
+    cv_translation = -1.0 * cv_world2cam_rot @ cam_location
+
+    blender_world2cam_rot = R_bcam2cv @ cv_world2cam_rot
+    blender_translation = R_bcam2cv @ cv_translation
+
+    blender_cam2world_rot = blender_world2cam_rot.transposed()
+    blender_cam_location = -1.0 * blender_cam2world_rot @ blender_translation
+
+    blender_matrix_world = Matrix(
+        (
+            blender_cam2world_rot[0][:] + (blender_cam_location[0],),
+            blender_cam2world_rot[1][:] + (blender_cam_location[1],),
+            blender_cam2world_rot[2][:] + (blender_cam_location[2],),
+            (0, 0, 0, 1),
+        )
+    )
+
+    return blender_matrix_world
+
+
+# Returns camera rotation and translation matrices from Blender.
+#
+# There are 3 coordinate systems involved:
+#    1. The World coordinates: "world"
+#       - right-handed
+#    2. The Blender camera coordinates: "bcam"
+#       - x is horizontal
+#       - y is up
+#       - right-handed: negative z look-at direction
+#    3. The desired computer vision camera coordinates: "cv"
+#       - x is horizontal
+#       - y is down (to align to the actual pixel coordinates
+#         used in digital images)
+#       - right-handed: positive z look-at direction
+def get_world2cam_from_blender_cam(cam):
+    # bcam stands for blender camera
+    R_bcam2cv = Matrix(((1, 0, 0), (0, -1, 0), (0, 0, -1)))
+
+    # Transpose since the rotation is object rotation,
+    # and we want coordinate rotation
+    # Use matrix_world instead to account for all constraints
+    location, rotation = cam.decompose()[
+        0:2
+    ]  # Matrix_world returns the cam2world matrix.
+    R_world2bcam = rotation.to_matrix().transposed()
+
+    # Convert camera location to translation vector used in coordinate changes
+    # T_world2bcam = -1*R_world2bcam*cam.location
+    # Use location from matrix_world to account for constraints:
+    T_world2bcam = -1 * R_world2bcam @ location
+
+    # Build the coordinate transform matrix from world to computer vision camera
+    R_world2cv = R_bcam2cv @ R_world2bcam
+    T_world2cv = R_bcam2cv @ T_world2bcam
+
+    # put into 3x4 matrix
+    RT = Matrix(
+        (
+            R_world2cv[0][:] + (T_world2cv[0],),
+            R_world2cv[1][:] + (T_world2cv[1],),
+            R_world2cv[2][:] + (T_world2cv[2],),
+            (0, 0, 0, 1),
+        )
+    )
+    return RT
